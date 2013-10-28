81f70ccf428593f66a1660fd17232aca2b8a1dde
JesperBaltzersen <jesperbaltzersen@gmail.com>
2013-10-28 17:22:15 +0100

    Commit IloginVindue og mapper

--- AbonnementsimuleringKlient/Indstillinger/IIndstillingerController.cs
+++ AbonnementsimuleringKlient/Indstillinger/IIndstillingerController.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace AbonnementsimuleringKlient
 {
-    interface IloginVindueController
+    interface IIndstillingerController
     {
-        public bool LoginVerificering();
     }
 }
--- AbonnementsimuleringKlient/Login/LoginVindue.cs
+++ AbonnementsimuleringKlient/Login/LoginVindue.cs
@@ -14,6 +14,7 @@ namespace AbonnementsimuleringKlient
     {
         public LoginVindue()
         {
+            //
             InitializeComponent();
         }
     }
