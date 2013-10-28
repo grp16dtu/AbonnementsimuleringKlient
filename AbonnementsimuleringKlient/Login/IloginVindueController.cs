78f1d7b2489d6f5b7b2be4c291149bfdd6086726
bjqrn88 <bjqrn1288>
2013-10-28 18:39:01 +0100

    Rename ILoginVindueController

--- AbonnementsimuleringKlient/Login/IloginVindueController.cs
+++ AbonnementsimuleringKlient/Login/IloginVindueController.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace AbonnementsimuleringKlient
 {
-    interface IloginVindueController
+    interface ILoginVindueController
     {
-        public bool LoginVerificering();
+       bool LoginVerificering();
     }
 }
