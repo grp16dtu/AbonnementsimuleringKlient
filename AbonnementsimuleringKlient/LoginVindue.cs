7970edf685b7cf6fc16c6394bbf0c51365fc7df7
JesperBaltzersen <jesperbaltzersen@gmail.com>
2013-10-28 16:36:05 +0100

    Created LoginVindue

--- AbonnementsimuleringKlient/LoginVindue.cs
+++ AbonnementsimuleringKlient/LoginVindue.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AbonnementsimuleringKlient
 {
-    public partial class Form1 : Form
+    public partial class LoginVindue : Form
     {
-        public Form1()
+        public LoginVindue()
         {
             InitializeComponent();
         }
