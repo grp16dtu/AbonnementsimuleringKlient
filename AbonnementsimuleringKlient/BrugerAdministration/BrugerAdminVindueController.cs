1ba3ecd25df67b569335912c570ffcb96e613974
Mads Slotsbo <mads.slotsbo@gmail.com>
2013-10-29 09:47:04 +0100

    BrugerAdminVindueController oprettet, og udfyldt

---
+++ AbonnementsimuleringKlient/BrugerAdministration/BrugerAdminVindueController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbonnementsimuleringKlient
+{
+    class BrugerAdminVindueController : IBrugerAdminVindueController
+    {
+        private BrugerAdminVindue brugerAdminVindue;
+        private IBrugerDAO iBrugerDAO;
+
+        public List<Medarbejder> Medarbejderlist { get; set; }
+
+
+        public void HentIndstillingerVindue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpretBruger()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SletBruger()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GemBruger()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MedarbejderFraListe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HentMedarbejderListe()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
