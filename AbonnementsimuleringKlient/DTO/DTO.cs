3200ec80a927a2abe1bd19fd8fe3ea88bb400c46
bjqrn88 <bjqrn1288>
2013-10-30 11:06:32 +0100

    IDTO og DTO oprettet

---
+++ AbonnementsimuleringKlient/DTO/DTO.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbonnementsimuleringKlient
+{
+    class DTO : IDTO
+    {
+        private HttpKlient httpKlient;
+
+        private IDTO iDTO;
+
+        public List<Medarbejder> HentMedarbejderList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SletMedarbejder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpretMedarbejder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RedigerMedarbejder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string[]> HentBrugsHistorik(TidsInterval tidsInterval, HistorikType historikType, Id id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> HentGrafDataDropdown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void KoerNySimulering()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Simuleringer> HentSimuleringsListe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HentEnkeltSimulering()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpdaterEConomicsCredentials(string aftalenr, string brugernavn, string kodeord)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AbonnementsimuleringKlient/DTO/IDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbonnementsimuleringKlient
+{
+    interface IDTO
+    {
+        List<Medarbejder> HentMedarbejderList();
+
+        void SletMedarbejder();
+
+        void OpretMedarbejder();
+
+        void RedigerMedarbejder();
+
+        List<string[]> HentBrugsHistorik(TidsInterval tidsInterval, HistorikType historikType, Id id);
+
+        List<string> HentGrafDataDropdown();
+
+        void KoerNySimulering();
+
+        List<Simuleringer> HentSimuleringsListe();
+
+        void HentEnkeltSimulering();
+
+        void OpdaterEConomicsCredentials(string aftalenr, string brugernavn, string kodeord);
+
+    }
+}
