0902124452e049a635c7ff051e683fb8e26573bd
JesperBaltzersen <jesperbaltzersen@gmail.com>
2013-10-31 15:06:33 +0100

    Tilføjet instantiering i main/program - simuleringsVindue fælles - IsimuleringsVindue, IsimuleringsDAO, simuleringsVindueController, SimuleringsController, SimuleringsDAO

---
+++ AbonnementsimuleringKlient/SimuleringsVindue/ISimuleringsDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbonnementsimuleringKlient
+{
+    interface ISimuleringsDAO
+    {
+        List<string> HentXakse();
+        List<double> HentYakse();
+        void GenererNySimulering();
+        List<string> HentSimuleringsListe();
+        List<string> Xakse { get; private set; }
+        List<double> Yakse { get; private set; }
+        void HentSimulering(SimuleringsVindue.xAkseKey xKey, SimuleringsVindue.yAkseKey yKey, string tidsStempel);
+    }
+}
---
+++ AbonnementsimuleringKlient/SimuleringsVindue/SimuleringsDAO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbonnementsimuleringKlient
+{
+    class SimuleringsDAO : ISimuleringsDAO
+    {
+        private IDTO DTO;
+        
+        public List<string> Xakse
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public List<double> Yakse
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public SimuleringsDAO()
+        {
+            this.DTO = DTO.Instance;    
+        }
+
+        public void GenererNySimulering()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> HentSimuleringsListe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HentSimulering(SimuleringsVindue.xAkseKey xKey, SimuleringsVindue.yAkseKey yKey, string tidsStempel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
