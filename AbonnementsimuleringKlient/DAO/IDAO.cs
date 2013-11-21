3a3d0c0e4465043cb303a8e348cb0a6ce482ce3a
Bjqrn88 <bjqrn1288>
2013-11-21 12:00:50 +0100

    så er vi klar igen

---
+++ AbonnementsimuleringKlient/DAO/IDAO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbonnementsimuleringKlient
+{
+    public interface IDAO
+    {
+        Task<List<BrugerDTO>> HentMedarbejderList();
+
+        bool SletMedarbejder(string brugernavn);
+
+        bool OpretMedarbejder(IBrugerDTO bruger);
+
+        bool RedigerMedarbejder(IBrugerDTO bruger);
+
+        void KoerNySimulering(int index);
+
+        Task<List<Datapunktsgruppering>> HentSimuleringsListe();
+
+        DatapunktLister HentEnkeltSimulering(int index);
+
+        ISimuleringsDTO HentNyesteSimulering(string xAkse, string yAkse);
+
+        IBrugerDTO LoginVerificering(string brugernavn, string kodeord);
+
+        bool OpretKonto(Konto konto);
+    }
+}
